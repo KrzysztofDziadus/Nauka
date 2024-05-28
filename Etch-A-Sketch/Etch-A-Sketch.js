2246539aa669448d5a5435a725386dd5f41cb99c
Krzysztof Dziaduś <krzysztofdziadus@gmail.com>
2024-05-28 18:46:06 +0200

    finish project

--- Etch-A-Sketch/Etch-A-Sketch.js
+++ Etch-A-Sketch/Etch-A-Sketch.js
@@ -1,35 +1,51 @@
 let buttonCreate = document.querySelector(".btnCreate");
 let gridBox = document.querySelector(".grid");
-
-buttonCreate.addEventListener('click',()=> createVariables());
-let number = document.querySelector(".number")
-
+let number = document.querySelector(".number");
+let color = document.querySelector(".btnRandomColor");
+let clicked = false;
 number.addEventListener('click', () => {
-    while (gridBox.firstChild){
+    while (gridBox.firstChild) {
         gridBox.removeChild(gridBox.firstChild);
         number.value = '';
-    }})
-let accounts = [];
-function createVariables(){
+        clicked = false;
+    }
+})
+function getRandomNumber() {
+    return Math.floor(Math.random() * 255);
+}
 
+
+function createVariables() {
+    while (number.value > 100) {
+        alert('musisz podać liczbę mniejszą lub równą 100')
+        number.value = '';
+    }
     let multiple = (number.value * number.value)
-    let xBox = Math.sqrt(1048576/multiple)
+    let xBox = Math.sqrt(1048576 / multiple)
+    let accounts = [];
     for (let i = 0; i < multiple; ++i) {
         accounts[i] = `box${i}`;
         accounts[i] = document.createElement("div")
         accounts[i].classList.add("boxStyle", `div${i}`)
-        accounts[i].style.width=`${xBox}px`;
-        accounts[i].style.height=`${xBox}px`;
+        accounts[i].style.width = `${xBox}px`;
+        accounts[i].style.height = `${xBox}px`;
         gridBox.appendChild(accounts[i])
+        if (clicked === true){
+            accounts[i].addEventListener('mouseover', () => {
+                const colorRandom = `rgba(${getRandomNumber()},${getRandomNumber()},${getRandomNumber()},1)`;
+                accounts[i].style.backgroundColor = colorRandom;
+                accounts[i].style.borderColor =  colorRandom;
+            });
+        }else{
+            accounts[i].addEventListener('mouseover', () => {
+                accounts[i].style.backgroundColor = `black`;
+                accounts[i].style.borderColor = 'black';
+            });
+        }
 
-        accounts[i].addEventListener('mouseover', () => {
-            console.log(accounts[i].style.backgroundColor='red');
-        });
     }
 }
 
-// gridBox.addEventListener('mouseover',() => colorBox())
-// function colorBox(e){
-//     console.log(e.target.querySelector(accounts[1]))
-//
-// }
+
+color.addEventListener('click', () => {clicked = true})
+buttonCreate.addEventListener('click', () => createVariables());
